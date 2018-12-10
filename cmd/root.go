eea848560deb4f85c4e4dc5b01df278c9ebc077b
Dave Greene <davepgreene@gmail.com>
2018-12-10 16:58:12 -0500

    Tie it all together with HTTP handling and the CLI interface.

---
+++ cmd/root.go
@@ -0,0 +1,124 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/davepgreene/slackmac/config"
+	"github.com/davepgreene/slackmac/errors"
+	"github.com/davepgreene/slackmac/http"
+	"github.com/davepgreene/slackmac/utils"
+	"os"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+var verbose bool
+
+// SlackMacCommand represents the base command when called without any subcommands
+var SlackMacCommand = &cobra.Command{
+	Use:   "slackmac",
+	Short: "A tiny HTTP proxy that validates Slack payloads.",
+	Long: `SlackMac is a tiny HTTP proxy that validates Slack payloads.
+I wrote this because Spring Boot has an issue where it is impossible
+to get the raw request body parameters in the correct order if a POST
+request is sent with Content-Type application/x-www-form-urlencoded.
+By offloading this work to a proxy, SlackMac can be dropped in front
+of any service that needs to validate Slack payloads without the developer
+ever having to worry about calculating HMACs. It's already done!`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := initializeConfig()
+		initializeLog()
+		if err != nil {
+			return err
+		}
+
+		// Prevent boot if we aren't using a supported algorithm
+		confAlg := viper.GetString("slack.algorithm")
+		if val, ok := config.SUPPORTED_ALGORITHMS[strings.ToUpper(confAlg)]; ok {
+			// Set the actual crypto algorithm instead of a string representation
+			viper.Set("slack.algorithm", val)
+			return boot()
+		}
+
+		supported := strings.Join(utils.MapKeys(config.SUPPORTED_ALGORITHMS), ", ")
+		message := fmt.Sprintf("Slack currently supports the following encryption algorithms: %s. You specified %s.", supported, confAlg)
+		err = errors.NewUnsupportedAlgorithmError(message, strings.ToUpper(confAlg))
+		log.Fatal(err)
+		return err
+	},
+}
+
+func boot() error {
+	// Instantiate our metrics client early
+	_, err := utils.Metrics()
+	if err != nil {
+		log.Error(err)
+	}
+	main := http.Handler()
+	log.Error(main)
+	return main
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the SlackMacCmd.
+func Execute() {
+	if err := SlackMacCommand.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	SlackMacCommand.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file")
+	SlackMacCommand.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose level logging")
+	validConfigFilenames := []string{"json", "toml"}
+	err := SlackMacCommand.PersistentFlags().SetAnnotation("config", cobra.BashCompFilenameExt, validConfigFilenames)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initializeLog() {
+	log.RegisterExitHandler(func() {
+		log.Info("Shutting down")
+	})
+
+	// Set logging options based on config
+	log.SetLevel(utils.GetLogLevel())
+
+	// If using verbose mode, log at debug level
+	if verbose {
+		log.Info("Verbose mode specified. Setting log level to DEBUG")
+		log.SetLevel(log.DebugLevel)
+	}
+
+	if viper.GetBool("log.json") {
+		log.SetFormatter(&log.JSONFormatter{})
+	}
+
+	if cfgFile != "" {
+		log.WithFields(log.Fields{
+			"file": viper.ConfigFileUsed(),
+		}).Info("Loaded config file")
+	}
+
+}
+
+func initializeConfig(subCmdVs ...*cobra.Command) error {
+	config.Defaults()
+
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+
+		// If a config file is found, read it in.
+		if err := viper.ReadInConfig(); err != nil {
+			return err
+		}
+	}
+	viper.AutomaticEnv() // read in environment variables that match
+
+	return nil
+}
---
+++ http/handler.go
@@ -0,0 +1,90 @@
+package http
+
+import (
+	"crypto"
+	"fmt"
+	"github.com/davepgreene/slackmac/store"
+	"github.com/davepgreene/slackmac/utils"
+	"github.com/gorilla/mux"
+	"github.com/meatballhat/negroni-logrus"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/thoas/stats"
+	"github.com/urfave/negroni"
+	"net/http"
+)
+
+//var statsMiddleware *stats.Stats
+
+// Handler returns an http.Handler for the API.
+func Handler() error {
+	storeConf := viper.GetStringMapString("store")
+	dataStore, err := store.CreateStore(storeConf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := mux.NewRouter()
+	statsMiddleware := stats.New()
+
+	// Add middleware handlers
+	n := negroni.New()
+
+	// Add recovery handler that logs
+	recovery := negroni.NewRecovery()
+	recovery.PrintStack = false
+	n.Use(recovery)
+
+	if viper.GetBool("log.requests") {
+		n.Use(negronilogrus.NewCustomMiddleware(utils.GetLogLevel(), utils.GetLogFormatter(), "requests"))
+	}
+
+	n.Use(statsMiddleware)
+
+	// Collect some metrics about incoming and active requests
+	n.Use(negroni.HandlerFunc(metricsMiddleware))
+
+	// Correlation id middleware
+	if viper.GetBool("correlation.enable") {
+		n.Use(negroni.HandlerFunc(correlationMiddleware(viper.GetStringMap("correlation"))))
+	}
+
+	// Validate headers and timestamp
+	skew := viper.GetDuration("slack.skew")
+	n.Use(negroni.HandlerFunc(timestamp(skew)))
+
+	// Validate the signature
+	algorithm := viper.Get("slack.algorithm")
+	n.Use(negroni.HandlerFunc(signature(dataStore, algorithm.(crypto.Hash))))
+
+	// All checks passed, forward the request
+	forwardConn := fmt.Sprintf("%s%s:%d", viper.GetString("service.protocol"), viper.GetString("service.hostname"), viper.GetInt("service.port"))
+	p := proxy(forwardConn)
+	r.PathPrefix("/").Handler(p)
+
+	n.UseHandler(r)
+
+	// Set up connection
+	conn := fmt.Sprintf("%s:%d", viper.GetString("listen.bind"), viper.GetInt("listen.port"))
+	log.Infof("Listening on %s", conn)
+
+	// Bombs away!
+	go func() {
+		m := mux.NewRouter()
+		m.HandleFunc("/stats", newAdminHandler(statsMiddleware).ServeHTTP)
+		adminConn := fmt.Sprintf("%s:%d", viper.GetString("admin.host"), viper.GetInt("admin.port"))
+		log.Fatal(server(adminConn, m).ListenAndServe())
+	}()
+	return server(conn, n).ListenAndServe()
+}
+
+func server(conn string, handler http.Handler) *http.Server {
+	srv := &http.Server{
+		Addr:    conn,
+		Handler: handler,
+	}
+
+	srv.SetKeepAlivesEnabled(false)
+
+	return srv
+}
